45854a3292d01d5f29a8c0e53b998b1f1ab01499
Captain Shadows <captain_shadows@aol.com>
2013-08-26 19:47:12 -0400

    meh

--- common/ccm/ban_death/utils/PlayerTracker.java
+++ common/ccm/ban_death/utils/PlayerTracker.java
@@ -2,7 +2,6 @@ package ccm.ban_death.utils;
 
 import static ccm.ban_death.BanOnDeath.server;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -16,32 +15,27 @@ public class PlayerTracker {
 
 	@ForgeSubscribe
 	public void handleDeath(final LivingDeathEvent evt) {
-		// evt.entityPlayer
 		if (evt.entityLiving instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) evt.entityLiving;
 			if (server != null) {
-				if (!server.isSinglePlayer()) {
-					if (!Properties.BAN_ADMINS) {
-						// if (NOT ADMIN) {
-						ban(player);
-						// }
-					} else {
+				if (!Properties.BAN_ADMINS) {
+					if (!FunctionHelper.isPlayerOp(player.username)) {
 						ban(player);
 					}
+				} else {
+					ban(player);
 				}
 			}
 		}
 	}
 
 	private static void ban(final EntityPlayer player) {
-		if (player instanceof EntityPlayerMP) {
-			StringBuilder sb = new StringBuilder();
-			// replace with ban for actual baning... TODO Implement it so it kicks and then blacklist you >:D
-			sb.append("/kick ");
-			sb.append(player.username);
-			sb.append(" ");
-			sb.append(Properties.BAN_REASON);
-			server.executeCommand(sb.toString());
-		}
+		StringBuilder sb = new StringBuilder();
+		// TODO Implement it so it kicks and then blacklist you >:D
+		sb.append("/ban ");
+		sb.append(player.username);
+		sb.append(" ");
+		sb.append(Properties.BAN_REASON);
+		server.executeCommand(sb.toString());
 	}
 }
