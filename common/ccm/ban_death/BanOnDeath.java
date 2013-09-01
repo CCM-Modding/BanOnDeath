d583db0246861de8bfcc600f8deb1d34c66ea6e9
Captain Shadows <captain_shadows@aol.com>
2013-08-31 22:28:04 -0400

    ...
    Signed-off-by: Captain Shadows <captain_shadows@aol.com>

--- common/ccm/ban_death/BanOnDeath.java
+++ common/ccm/ban_death/BanOnDeath.java
@@ -8,12 +8,12 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 import ccm.ban_death.utils.PlayerTracker;
 import ccm.ban_death.utils.lib.Archive;
-import ccm.nucleum.omnium.BaseMod;
+import ccm.nucleum.omnium.CCMMod;
 import ccm.nucleum.omnium.IMod;
 import ccm.nucleum.omnium.utils.handler.config.ConfigurationHandler;
 
 @Mod(modid = Archive.MOD_ID, name = Archive.MOD_NAME)
-public class BanOnDeath extends BaseMod implements IMod
+public class BanOnDeath extends CCMMod implements IMod
 {
 
     @Instance
